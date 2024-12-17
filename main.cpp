e07bcccee58eb7fb9d90a9f8bf96d6781dcbf0ae
Kolin63 <theverylastgoodname@gmail.com>
2024-12-17 17:50:09 -0500

    Added New Line

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     std::cout << "Welcome to C++man (a variant of Hangman)\n";
-    std::cout << "To win: guess the word.  To lose: run out of pluses.\n";
+    std::cout << "To win: guess the word.  To lose: run out of pluses.\n\n\n";
 
     Session s{ 6 };
 
